aa293fd5ff15929cec68880cf5243621b1fdd817
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-27 23:01:27 +0700

    Add name to run

--- python/main.py
+++ python/main.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n-cls.yaml")
 
-model.train(data="dataset/", epochs=100, imgsz=640)
-# model.train(data="mnist160", epochs=1000, imgsz=64)
+model.train(data="dataset/", epochs=100, imgsz=640, name="hundredEpochDefaultYolo")
+# model.train(data="mnist160", epochs=10, imgsz=64, name="mnistCheck")
