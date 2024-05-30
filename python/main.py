53e768eb878bd4df928ddb44ca94382ac6a03aa9
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-31 03:50:30 +0700

    Clean up main.py

--- python/main.py
+++ python/main.py
@@ -1,35 +1,12 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from pytorch_metric_learning import losses
 from ultralytics.nn.tasks import ClassificationModel
 from ultralytics.models.yolo.classify import ClassificationTrainer
 
 
-class ArcFace(nn.Module):
-    def __init__(self, feature_in, feature_out=24, margin=0.3, scale=64):
-        super().__init__()
-        self.feature_in = feature_in
-        self.feature_out = feature_out
-        self.scale = scale
-        self.margin = margin
-
-        self.weights = nn.Parameter(torch.FloatTensor(feature_out, feature_in))
-        nn.init.xavier_normal_(self.weights)
-
-    def forward(self, features, targets):
-        cos_theta = F.linear(features, F.normalize(self.weights), bias=None)
-        cos_theta = cos_theta.clip(-1 + 1e-7, 1 - 1e-7)
-        arc_cos = torch.acos(cos_theta)
-        arc_cos += F.one_hot(targets, num_classes=self.feature_out) * self.margin
-        cos_theta_2 = torch.cos(arc_cos)
-        return cos_theta_2 * self.scale
-
-
 class Loss:
     def __call__(self, preds, batch):
-        #     arch_face = ArcFace(feature_in=batch.get_shape(), feature_out=100)
-        #     return arch_face.forward(batch, preds)
-        loss = F.cross_entropy(preds, batch["cls"], reduction="mean")
+        loss_func = losses.ArcFaceLoss(num_classes=100, embedding_size=preds.shape[1])
+        loss = loss_func(preds, batch['cls'])
         loss_items = loss.detach()
         return loss, loss_items
 
@@ -46,9 +23,9 @@ class CustomTrainer(ClassificationTrainer):
 
 params = {
     'data': '../dataset_top_100_authors/',
-    'epochs': 3,
+    'epochs': 50,
     'imgsz': 640,
-    'name': 'trainer_n_3_epoch',
+    'name': 'arc_face_n_50',
     'model': 'yolov8n-cls.yaml'
 }
 trainer = CustomTrainer(overrides=params)
